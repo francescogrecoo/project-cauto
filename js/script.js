51871aceebf243eafb485e8b4e3087e11619528c
francescogrecoo <francesco.greco.priv@gmail.com>
2021-05-12 21:12:31 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//backtotop button
+let btnGo = document.querySelector('.contGo');
+
+window.addEventListener('scroll' , () => {
+    if(window.scrollY > 200){
+        btnGo.style.display = "block";
+        btnGo.style.animation = "scale-in-center 0.2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
+    } else{
+        btnGo.style.animation = "scale-out 0.2s cubic-bezier(0.250, 0.460, 0.450, 0.940) reverse both"
+    }
+})
+
+function onTop(){
+    window.scrollTo(0,0);
+}
